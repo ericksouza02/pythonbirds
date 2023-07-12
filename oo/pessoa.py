329630e2b9561de12fe19d3a39e30a79aac30c7f
Erick <erick_souza2104@outlook.com>
2023-07-12 14:47:08 -0600

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome, idade):
         self.nome = nome
         self.idade = idade
@@ -19,4 +20,12 @@ if __name__ == '__main__':
     del erick.filhos
     print(erick.__dict__)
     print(renzo.__dict__)
-
+    print(id(erick.olhos))
+    print(id(Pessoa.olhos))
+    print(erick.__dict__)
+    print(renzo.__dict__)
+    print('-----------------')
+    Pessoa.olhos = 5
+    print(erick.olhos)
+    renzo.olhos = 2
+    print(renzo.olhos)
